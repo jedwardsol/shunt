c5f8fe3ed7fa24bb298b077f3338648df7a1c098
jedwards <jonathan_edwards@mcafee.com>
2022-01-13 07:45:18 -0800

    ( )

--- shunt.cpp
+++ shunt.cpp
@@ -27,27 +27,42 @@ struct Parse
 
 };
 
-std::map<char, Parse> parse
+std::map<std::string, Parse> parse
 {
-    { '^'   , {3, static_cast<OpPointer>(std::pow)} },
-    { '*'   , {2, std::multiplies<double>{} }},
-    { '/'   , {2, std::divides<double>{}    }},
-    { '+'   , {1, std::plus<double>{}       }},
-    { '-'   , {1, std::minus<double>{}      }},
+    { "^^"  , {3, static_cast<OpPointer>(std::pow)} },             // ^ is an escape character in cmd
+    { "^"   , {3, static_cast<OpPointer>(std::pow)} },
+    { "*"   , {2, std::multiplies<double>{} }},
+    { "/"   , {2, std::divides<double>{}    }},
+    { "+"   , {1, std::plus<double>{}       }},
+    { "-"   , {1, std::minus<double>{}      }},
+
+
 };
 
 
 
 
-bool isOperator(char c)
+bool isOperator(std::string const &c)
 {
     return parse.contains(c);    
 }
 
+bool isOpen(std::string const &c)
+{
+    return c == "(";    
+}
+
+bool isClose(std::string const &c)
+{
+    return c == ")";    
+}
+
+
+
 RPN shunt(std::vector<std::string> const &tokens)
 {
-    RPN                     rpn;
-    std::stack<char>        operators;
+    RPN                         rpn;
+    std::stack<std::string>     operators;
 
     auto transferOperator = [&]
     {
@@ -57,22 +72,38 @@ RPN shunt(std::vector<std::string> const &tokens)
         rpn.push_back( parse[symbol].op);
     };
 
-
-
     for(auto const &token : tokens)
     {
-        auto const symbol = token[0];
-
-        if(isOperator(symbol))
+        if(isOpen(token))
+        {
+            operators.push(token);
+        }
+        else if(isClose(token))
         {
             while(   !operators.empty()
-                  &&  parse[operators.top()].precedence >= parse[symbol].precedence)
+                  && !isOpen(operators.top()))
             {
                 transferOperator();
             }
 
+            if(operators.empty())
+            {
+                throw_runtime_error("Mismatched parentheses");
+            }
+
+            operators.pop();
+
+        }
+        else if(isOperator(token))
+        {
+            while(   !operators.empty()
+                  && !isOpen(operators.top())
+                  &&  parse[operators.top()].precedence >= parse[token].precedence)
+            {
+                transferOperator();
+            }
             
-            operators.push(symbol);
+            operators.push(token);
         }
         else
         {
@@ -85,7 +116,6 @@ RPN shunt(std::vector<std::string> const &tokens)
     {
         transferOperator();
     }
-
 
     return      rpn;
 }
