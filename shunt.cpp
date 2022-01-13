d136732d3abeadfc47a4ecc959c3ff35ef0a5d72
Jonathan Edwards <github@jonstuff.com>
2022-01-12 22:27:52 -0800

    Shunt a bit

--- shunt.cpp
+++ shunt.cpp
@@ -3,21 +3,58 @@
 #include <vector>
 #include <queue>
 #include <stack>
+#include <map>
+
 #include <string>
 #include <variant>
-#include <memory>
 #include <functional>
 #include <cmath>
+#include <limits>
 
 using namespace std::literals;
 #include "thrower.h"
 #include "print.h"
 
 
-using Operation = std::function<double(double,double)> ;
-using Token    = std::variant<double, Operation >;
+using Operator  = std::function<double(double,double)> ;
+using Token     = std::variant<double, Operator >;
+using RPN       = std::vector<Token>;
 
-double rpn(std::vector<Token> const  &tokens)
+
+double power(double lhs, double rhs)
+{
+    return std::pow(lhs,rhs);
+}
+
+double NaN(double lhs, double rhs)
+{
+    return std::numeric_limits<double>::quiet_NaN();
+}
+
+struct Parse
+{
+    int         precedence;
+    Operator    op;
+
+    Parse(int precedence, Operator op) : precedence{precedence}, op{op}
+    {}
+
+};
+
+std::map<char, Parse> precedence
+{
+    { '('   , {4, NaN} },
+    { ')'   , {4, NaN} },
+    { '^'   , {3, power} },
+    { '*'   , {2, std::multiplies<double>{} }},
+    { '/'   , {2, std::divides<double>{}    }},
+    { '+'   , {1, std::plus<double>{}       }},
+    { '-'   , {1, std::minus<double>{}      }},
+};
+
+
+
+double resolve(RPN  const  &tokens)
 {                     
     std::stack<double>   numbers;
 
@@ -31,9 +68,9 @@ double rpn(std::vector<Token> const  &tokens)
 
     for(auto const &token : tokens)
     {
-        if(token.index()==0)
+        if(std::holds_alternative<double>(token))
         {
-            numbers.push( std::get<0>(token));
+            numbers.push( std::get<double>(token));
         }
         else
         {
@@ -44,7 +81,7 @@ double rpn(std::vector<Token> const  &tokens)
 
             auto right     = pop();
             auto left      = pop();
-            auto operation = std::get<1>(token);
+            auto operation = std::get<Operator>(token);
 
             numbers.push (operation(left,right));
         }
@@ -58,10 +95,6 @@ double rpn(std::vector<Token> const  &tokens)
     return numbers.top();
 }
 
-double power(double lhs, double rhs)
-{
-    return std::pow(lhs,rhs);
-}
 
 void testRPN()
 {
@@ -71,17 +104,86 @@ void testRPN()
     std::vector<Token>  ratio      {  6.0, 9.0, std::divides<double>{} };
     std::vector<Token>  exponent   {  6.0, 9.0, power };
                                               
-    print("sum        = {}\n",rpn(sum));
-    print("product    = {}\n",rpn(product));
-    print("difference = {}\n",rpn(difference));
-    print("ratio      = {}\n",rpn(ratio));
-    print("exponent   = {}\n",rpn(exponent));
+    print("sum        = {}\n",resolve(sum));
+    print("product    = {}\n",resolve(product));
+    print("difference = {}\n",resolve(difference));
+    print("ratio      = {}\n",resolve(ratio));
+    print("exponent   = {}\n",resolve(exponent));
 }
+
+bool isOperator(char c)
+{
+    return precedence.contains(c);    
+}
+
+auto shunt(std::vector<std::string> const &tokens)
+{
+    RPN                     rpn;
+    std::stack<char>        operators;
+
+    auto transferOperator = [&]
+    {
+        auto symbol = operators.top();
+        operators.pop();
+
+        switch(symbol)
+        {
+        case '+':
+            rpn.push_back(std::plus<double>{});
+            break;
+
+        case '-':
+            rpn.push_back(std::minus<double>{});
+            break;
+
+
+
+        }
+
+    };
+
+
+
+    for(auto const &token : tokens)
+    {
+        auto const symbol = token[0];
+
+        if(isOperator(symbol))
+        {
+            
+            operators.push(symbol);
+        }
+        else
+        {
+            rpn.push_back(std::stod(token));
+        }
+    }
+
+
+    while(!operators.empty())
+    {
+        transferOperator();
+    }
+
+
+    return      rpn;
+}
+
 
 int main(int argc, char const *argv[])
 try
 {
-    testRPN();
+    std::vector<std::string>  args(argv+1,argv+argc);
+
+    if(args.empty())
+    {
+        throw_runtime_error("No calculation");
+    }
+
+    auto rpn = shunt(args);
+
+    print("Result = {}\n",resolve(rpn));
+    
 
     return 0;
 }
